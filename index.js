3a4d9360e256246318409997558d50d38eab392d
LRojon <loic.rojon@gmail.com>
2021-09-22 15:11:38 +0200

    End of Creatures CRUD

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const creatures = require('./routes/CreatureController')
 
 app.use(express.json())
 app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    res.header("Content-Type: application/json")
+    next()
+})
 app.use('/creatures', creatures)
 
 app.listen(5500, () => {console.log("App started, and listen on port 5500.")})
--- routes/CreatureController.js
+++ routes/CreatureController.js
@@ -3,27 +3,81 @@ const router = express.Router()
 
 const CreatureModel= require('../models/creatureModel')
 
-router.get('/all', (req, res) => {
+router.get('/get/all', (req, res) => { // Get All
     CreatureModel.find({}, (err, docs) => {
         if(err) { console.log(err) }
         else {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-            res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
             res.send(docs) 
         }
     })
 })
 
-router.post('/post/one', (req, res) => {
-    CreatureModel.create(req.body, (err) => {
+router.get('/get/all/name', (req, res) => { // Get All Names
+    CreatureModel.find({}, (err, docs) => {
         if(err) { console.log(err) }
-        else { 
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-            res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-            res.header("Content-Type: application/json")
-            res.status(201).send('Created successfully.') 
+        else {
+            let names = []
+            docs.forEach(doc => {
+                names.push(doc.name)
+            })
+            res.send(names)
+        }
+    })
+})
+
+router.get('/get/id/:id', (req, res) => {
+    if(req.params.id.length != 24) { res.status(404).send('Wrong id') }
+    CreatureModel.findOne({ _id: req.params.id }, (err, doc) => {
+        if(err) { console.log(err) }
+        else {
+            if(doc != null) { res.send(doc) }
+            else { 
+                res.status(404)
+                res.send('Wrong id') 
+            }
+        }
+    })
+})
+
+router.get('/get/name/:name', (req, res) => {
+    CreatureModel.findOne({ name: req.params.name }, (err, doc) => {
+        if(err) { console.log(err) }
+        else {
+            if(doc != null) { res.send(doc) }
+            else { res.status(404).send('Wrong name') }
+        }
+    })
+})
+
+router.post('/set/one', (req, res) => { // Create One / Update One
+    CreatureModel.find({ name: req.body.name}, (err, docs) => {
+        if(err) { console.log(err) }
+        else {
+            if(docs.length != 0) {
+                CreatureModel.replaceOne({name: req.body.name}, req.body, (err, doc) => {
+                    if(err) { console.log(err) }
+                    else { res.status(200).send('Updated successfully.') }
+                })
+            }
+            else {
+                CreatureModel.create(req.body, (err) => {
+                    if(err) { console.log(err) }
+                    else {
+                        res.status(201).send('Created successfully.') 
+                    }
+                })
+            }
+        }
+    })
+})
+
+router.get('/del/one/:id', (req, res) => {
+    if(req.params.id.length != 24) { res.send(404).send('Wrong id') }
+    CreatureModel.deleteOne({_id: req.params.id}, (err, result) => {
+        if(err) { console.log(err) }
+        else {
+            if(result.deletedCount != 0) { res.status(200).send('Deleted successfully.') }
+            else { res.status(404).send('Wrong id') }
         }
     })
 })
