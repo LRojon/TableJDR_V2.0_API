a47adf5d1f4e8957c68bfbf6ff4e13060f1d6476
LRojon <loic.rojon@gmail.com>
2021-10-20 13:43:21 +0200

    Modify for prod

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -4,8 +4,8 @@ require('dotenv').config()
 const url = "mongodb://" + process.env.DB_USER + ':' + process.env.DB_PWD + "@mongodb-lrojon.alwaysdata.net/" + process.env.DB_ROOT
 
 mongoose.connect(
-    "mongodb://127.0.0.1:27017/HnD",
-    //url,
+    //"mongodb://127.0.0.1:27017/HnD",
+    url,
     { useNewUrlParser: true, useUnifiedTopology: true },
     (err) => {
         if(err) console.log(err + url);
